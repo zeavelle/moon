319a5bb2e49b7ad6484a5c3be4fa75cbb138ee6b
Aftiyan <aftiyangamer45@gmail.com>
2024-03-17 22:47:49 +0700

    [ADD tambah asset background

--- app/page.jsx
+++ app/page.jsx
@@ -10,14 +10,9 @@ import Star from "./components/star";
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 export default function Home() {
-  const [render, setRender] = useState(0);
-
   const tl = useRef();
   const titleRef = useRef();
   const descRef = useRef();
-  const starRef1 = useRef();
-  const starRef2 = useRef();
-  const starRef3 = useRef();
 
   useGSAP(() => {
     const titleText = new SplitType(titleRef.current);
@@ -26,65 +21,121 @@ export default function Home() {
     tl.current = gsap
       .timeline()
       .from(titleText.chars, {
+        y: 70,
         opacity: 0,
-        y: 30,
         duration: 1,
         delay: 0.5,
+        ease: "back.out(1.7)",
         stagger: {
-          amount: 0.5,
+          amount: 0.7,
         },
       })
-      .from(descText.chars, {
-        y: -20,
-        opacity: 0,
-        duration: 1,
-        stagger: {
-          amount: 0.3
-        }
-      }, "-=1")
-      .from(starRef1.current, {
-        x: -50,
-        opacity: 0,
-        rotate: 360,
-        duration: 1
-      }, "-=1.5")
-      .from(starRef2.current, {
-        y: -50,
-        x: 50,
-        opacity: 0,
-        scale: 0,
-        rotate: -270,
-        duration: 1
-      }, "-=1.5")
-      .from(starRef3.current, {
-        y: -50,
-        x: -50,
-        opacity: 0,
-        scale: 0,
-        rotate: 180,
-        duration: 1
-      }, "-=1.3");
+      .from(
+        descText.chars,
+        {
+          y: 70,
+          duration: 1,
+          opacity: 0,
+          ease: "back.out(1.7)",
+          stagger: {
+            amount: 0.7,
+          },
+          onComplete: () => {
+            gsap.to(".line", {
+              scaleX: 1,
+              duration: 0.5,
+            });
+          },
+        },
+        "-=1.5"
+      )
+      .from(
+        ".star-1",
+        {
+          x: -50,
+          opacity: 0,
+          rotate: 360,
+          duration: 1,
+        },
+        "-=1.5"
+      )
+      .from(
+        ".star-2",
+        {
+          y: -50,
+          x: 50,
+          opacity: 0,
+          scale: 0,
+          rotate: -270,
+          duration: 1,
+        },
+        "-=1.5"
+      )
+      .from(
+        ".star-3",
+        {
+          y: -50,
+          x: -50,
+          opacity: 0,
+          scale: 0,
+          rotate: 180,
+          duration: 1,
+          onComplete: () => {
+            gsap.to(".star-1", {
+              rotate: -345,
+              scrollTrigger: {
+                trigger: ".desc",
+                start: "bottom center",
+                end: "bottom top",
+                scrub: 1.5,
+              },
+            });
+            gsap.to(".star-2", {
+              rotate: 210,
+              scrollTrigger: {
+                trigger: ".desc",
+                start: "bottom center",
+                end: "bottom top",
+                scrub: 1.5,
+              },
+            });
+            gsap.to(".star-3", {
+              rotate: -135,
+              scrollTrigger: {
+                trigger: ".desc",
+                start: "bottom center",
+                end: "bottom top",
+                scrub: 1.5,
+              },
+            });
+          },
+        },
+        "-=1.3"
+      );
   }, null);
 
   return (
-    <div className="h-[200vh] overflow-x-hidden">
-      {/* <button
-        className="fixed top-2 left-2 bg-indigo-700 py-1 px-3 rounded text-sm opacity-20 z-50"
-        onClick={() => setRender((prev) => prev + 1)}
-      >
-        Render = {render}
-      </button> */}
-      <div className="h-[100svh] flex flex-col justify-center px-6 relative">
-        <Star className="fill-gray-50 w-10 top-56" ref={starRef1}/>
-        <Star className="fill-gray-950 w-6 top-[17rem] right-10" stroke="#818cf8" ref={starRef2}/>
-        <Star className="fill-indigo-400 w-10 bottom-[17rem] right-7" ref={starRef3} />
-        <div className="flex flex-col self-start w-full mb-2 overflow-y-hidden title-wrapper" ref={titleRef}>
-          <p className="col-span-2 text-6xl">Marhaban</p>
-          <p className="col-span-2 text-[2.8rem] whitespace-nowrap ms-auto -mt-1 !text-end">Ya <span className="text-indigo-400">Ramadhan</span></p>
+    <div className="h-[1000vh] overflow-x-hidden">
+      <div className="h-[100svh] flex flex-col justify-center px-6 relative wrapper">
+        <Star className="fill-indigo-300 w-10 top-56 star-1" />
+        <Star className="fill-gray-950 w-6 top-[17rem] right-10 star-2" stroke="#f9fafb" />
+        <Star className="fill-orange-400 w-10 bottom-[17rem] right-7 star-3" />
+        <div className="header">
+          <div className="flex flex-col self-start w-full mb-2 title-wrapper" ref={titleRef}>
+            <p className="col-span-2 text-6xl marhaban">Marhaban</p>
+            <p className="col-span-2 text-[2.8rem] whitespace-nowrap ms-auto -mt-1 !text-end ya-ramadhan">
+              Ya <span className="text-indigo-300">Ramadhan</span>
+            </p>
+          </div>
+          <p className="max-w-60 overflow-y-hidden font-medium leading-snug desc" ref={descRef}>
+            May{" "}
+            <span className="text-orange-400 relative">
+              <span className="line absolute bottom-0 left-0 w-full h-[2px] scale-x-0 origin-left bg-orange-400"></span>
+              Allah SWT
+            </span>{" "}
+            always grant safety in this blessed month
+          </p>
         </div>
-        <p className="max-w-60 overflow-y-hidden leading-snug" ref={descRef}>
-          May <span className="text-indigo-400">Allah SWT</span> always grant safety in this blessed month
-        </p>
       </div>
     </div>
   );
